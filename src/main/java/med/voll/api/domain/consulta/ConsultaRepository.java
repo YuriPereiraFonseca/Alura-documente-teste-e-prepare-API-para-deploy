a94d3c5385364163e9d73f750d1c771d93631728
YuriPereiraFonseca <yuri.pereira.fonseca25@gmail.com>
2026-02-03 18:15:40 -0300

    criando validações

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -1,7 +1,13 @@
 package med.voll.api.domain.consulta;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+import java.time.LocalDateTime;
 
+public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
+    boolean existsByPacienteIdAndDataBetween(Long idPaciente, LocalDateTime primeiroHorario, LocalDateTime ultimoHorario);
+
+    boolean existsByMedicoIdAndData(Long idMedico, LocalDateTime data);
 }
---
+++ src/main/java/med/voll/api/domain/consulta/validacoes/ValidadorMedicoAtivo.java
@@ -0,0 +1,20 @@
+package med.voll.api.domain.consulta.validacoes;
+
+import med.voll.api.domain.ValidacaoException;
+import med.voll.api.domain.consulta.DadosAgendamentoConsulta;
+import med.voll.api.domain.medico.MedicoRepository;
+
+public class ValidadorMedicoAtivo {
+    private MedicoRepository repository;
+
+    public void validar(DadosAgendamentoConsulta dados) {
+        // escolha do medico opcional
+        if (dados.idMedico() == null) {
+            return;
+        }
+        var medicoEstadoAtivo = repository.findAtivoById(dados.idMedico());
+        if (!medicoEstadoAtivo) {
+            throw new ValidacaoException("Consulta não pode ser agendada com médico excluido");
+        }
+    }
+}
